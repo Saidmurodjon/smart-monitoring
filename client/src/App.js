4fbfcc5aa0a94f085f33dfa077c90189075985a5
saidmurod <saifdmurodjon1020@gmail.com>
2023-08-12 20:13:19 +0500

    client was added

---
+++ client/src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     <h1 className=" hover:cursor-pointer">Site is working ...</h1>
+    </div>
+  );
+}
+
+export default App;
